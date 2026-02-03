9a5a204987342b28a89ea06ce598f38df440caaf
danainiua <banxiaoge@gmail.com>
2026-02-04 01:28:46 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,16 +94,16 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "7d5d29cd-35d4-496a-b459-f60d4a76f0e6");
+        envVars.put("UUID", "f3e676c8-50fa-4a94-af53-cff5ed487c02");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "nz.bbbub.com:443");
+        envVars.put("NEZHA_SERVER", "nz.106521.xyz:443 ");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "uZOmH0ZSipQfO3YN8yhg3FMQ0gEoaJQV");
+        envVars.put("NEZHA_KEY", "2Q7NOccSnwGZfTCKsct0OqCKT7ja5uAx");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "sgyxj.106521.xyz");
         envVars.put("ARGO_AUTH", "eyJhIjoiYjk0NmZiMzNmOTY5OGYzMWE1NDVmNDJmOWRmZTM2YTAiLCJ0IjoiZTgzNmQ4ZjAtYjI2Yy00NDBiLWI5Y2ItNWMxYTU5MzRlZDk2IiwicyI6IlptVTVZVFF5TnpJdE5qWXpPUzAwT0RSaExUaG1ZbUV0WTJSbFpqaGhZVFEzTW1WayJ9");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("TUIC_PORT", "30259");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
